ac12f79c26bbe7e400accf002cdb7fe2ee008204
shubhangi76779 <shubhangirathore3770@gmail.com>
2025-07-09 18:41:57 +0530

    Update api.ts

--- src/app/api.ts
+++ src/app/api.ts
@@ -2,6 +2,7 @@ import axios from "axios";
 
 export const api = axios.create({
     baseURL: import.meta.env.PROD
-        ? "https://blogappbackend-1-225y.onrender.com",
+       ? "https://blogappbackend-1-225y.onrender.com"
+        : "http://localhost:3000",
     withCredentials: true,
 });
