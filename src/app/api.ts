6b68b526bbb2749f26df8c7c356d7d232e51b5d7
shubhangi76779 <shubhangirathore3770@gmail.com>
2025-07-09 18:35:09 +0530

    Update api.ts

--- src/app/api.ts
+++ src/app/api.ts
@@ -2,7 +2,6 @@ import axios from "axios";
 
 export const api = axios.create({
     baseURL: import.meta.env.PROD
-        ? import.meta.env.VITE_BACKEND_SERVER_PROD
-        : "http://localhost:3000",
+        ? "https://blogappbackend-1-225y.onrender.com",
     withCredentials: true,
 });
